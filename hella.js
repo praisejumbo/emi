cd7f6884a41cc16fdc387cc71952926f0ed9a3bd
unknown <praisejumbo@gmail.com>
2021-04-06 09:21:20 +0100

    i just added few things

--- hella.js
+++ hella.js
@@ -1,8 +1,70 @@
+const http = require("http");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+var fs = require('fs')
+const path = require("path");
+const addReqUser = require("./");
 
-var http = require('http');
+// app.user(user);
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-    
-}).listen(8080); 
+app.get("/user", function (req,res,){
+
+  res.send("this is an example of routing using the get method");
+})
+
+app.post("/user", function (req,res,){
+
+  res.send("this is an example of routing using the post method");
+})
+
+app.delete("/user", function (req,res,){
+
+  res.send("this is an example of routing using the delete method");
+})
+
+
+
+// app.get('/', (req, res) => {
+//   // const pathToFile = path.join('my login form.html', "public", req.url);
+//   fs.readFile('./public/my login form.html', "utf8", function (err, data) {
+//   console.log(data);
+//   res.send(data);
+
+// })
+// })
+// if (err) {
+//   console.dir(err);
+//   res.writeHead(505, { "Content-Type": "text/plain" });
+//   return res.end("File Not Found");
+// }
+
+function addRequser(req,res,next){
+  req.user={name:"Emmanuel"};
+  next();
+}
+
+app.get("/addReqUser", addRequser, (req,res,)=>{
+
+  res.send(req.user.name);
+});
+
+// // app.get("/json", function(req,res){
+// //   constobj={message:}
+// })
+
+app.get("./about", function (req, res) {
+  res.send("this is the about page");
+});
+
+app.get("/home", function (req, res) {
+  res.send("this is the home page");
+});
+
+app.get("/login", function (req, res) {
+  res.send("this is the login page");
+});
+
+app.listen(3000, function () {
+  console.log("server is runing")
+});
